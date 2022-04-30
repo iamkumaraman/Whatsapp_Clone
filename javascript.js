ee71e85892c3d94fcb7c4df8f8445747ac9cc003
Amankumar-1999 <amankumarexcellent@gmail.com>
2022-04-30 10:01:59 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+alert(
+  "Hello Everyone!👋\nWelcome to my Marvelous project\n♎WhatsApp Clone♎ I hope you are doing well👌 This project is one of the best project I have ever made in my life."
+);
+alert(
+  "🤖FEATURES:🤖\n\n1: CHATS\n2: STATUS\n3: CALLS\n4: VIEW STATUS\n5: VIEW PROFILE PICTURE\n6: CHAT WITH PEOPLE\n7: CAMERA E.T.C\n\n"
+);
